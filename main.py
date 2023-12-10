50bfa06e9685d0b9e4ab8e34bae19f75e9492f55
peyronth <peyron.th@gmail.com>
2023-12-10 17:21:11 +0100

    Move supported website to another file

--- main.py
+++ main.py
@@ -1,14 +1,5 @@
-import scripts.chronorace as chronorace
-import scripts.kikourou as kikourou
-import scripts.sportmaniacs as sportmaniacs
 import os
-
-# Table referencing for each website, webSiteName, scriptPath and exampleUrl.
-supportedWebsites = {
-    'chronorace' : {"script": chronorace.chronorace, "exampleUrl": "https://prod.chronorace.be/Classements/Classement.aspx?eventId=1188318666566175&IdClassement=17573"},
-    'kikourou' : {"script": kikourou.kikourou, "exampleUrl": "http://www.kikourou.net/resultats/resultat-154252-la_course_nature_des_3_etangs_-_18_km-2020.html"},
-    'sportmaniacs' : {"script": sportmaniacs.sportmaniacs, "exampleUrl": "https://sportmaniacs.com/es/races/gtpe-2023-gran-trail-picos-de-europa/64976d6e-e580-48b6-ba51-6641ac1f1c02/results#rankings"}
-}
+from scriptlist import supportedWebsites as supportedWebsites
 
 def create_export_folder():
     export_folder = 'export'
