89a64c4b483019432890b4caae4f31ab1bcc36df
peyronth <peyron.th@gmail.com>
2023-12-14 20:27:57 +0100

    Add utmb world script

--- scriptlist.py
+++ scriptlist.py
@@ -3,11 +3,13 @@ import scripts.kikourou as kikourou
 import scripts.sportmaniacs as sportmaniacs
 import scripts.eventicom as eventicom
 import scripts.ultratiming as ultratiming
+import scripts.utmb as utmb
 
 supportedWebsites = {
     'chronorace' : {"script": chronorace.chronorace, "exampleUrl": "https://prod.chronorace.be/Classements/Classement.aspx?eventId=1188318666566175&IdClassement=17573"},
     'eventicom' : {"script": eventicom.eventicom, "exampleUrl": "https://eventicom.fr/resultats/TrailNoel2023/10%C3%A8me%20Trail%20de%20No%C3%ABl.clax" },
     'kikourou' : {"script": kikourou.kikourou, "exampleUrl": "http://www.kikourou.net/resultats/resultat-154252-la_course_nature_des_3_etangs_-_18_km-2020.html"},
     'sportmaniacs' : {"script": sportmaniacs.sportmaniacs, "exampleUrl": "https://sportmaniacs.com/es/races/gtpe-2023-gran-trail-picos-de-europa/64976d6e-e580-48b6-ba51-6641ac1f1c02/results#rankings"},
-    'ultratiming' : {"script": ultratiming.ultratiming, "exampleUrl": "https://www.ultratiming.be/evenement/trail-du-hoyoux-2023/epreuve/34km/resultats"}
+    'ultratiming' : {"script": ultratiming.ultratiming, "exampleUrl": "https://www.ultratiming.be/evenement/trail-du-hoyoux-2023/epreuve/34km/resultats"},
+    'utmb world' : {"script": utmb.utmbworld, "exampleUrl": "https://kosciuszko.utmb.world/runners/results?year=2023&raceUri=33948.ultra-trailkosciuszkobyutmbkoscimiler.2023"}
 }
