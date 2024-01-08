6e32f91037480541acaedc0b146781919b153056
peyronth <peyron.th@gmail.com>
2024-01-08 09:38:26 +0100

    Fix race result with dynamic time column.

--- tools/htmlParser/raceresult.py
+++ tools/htmlParser/raceresult.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import libs.excelExport as excelExport
 from bs4 import BeautifulSoup
+import tkinter as tk
 
 def raceresult(html): 
     #Parse html
@@ -12,14 +13,19 @@ def raceresult(html):
     # Table to panda dataframe
     df = pd.read_html(str(table))[0]
     
-    #Keep columns Bib, Name, Gender, Nat., YoB, Club, Time
-    df = df[["Bib", "Name", "Gender", "Nat.", "YoB", "Club", "Time"]]
+    # Ask for the name of the time column
+    root = tk.Tk()
+    root.withdraw()
+    time_column_name = ""
+    while time_column_name == "" or time_column_name not in df.columns:
+        time_column_name = tk.simpledialog.askstring("Time column name", "What is the name of the time column?")
 
     # Remove row with Time NaN
-    df = df.dropna(subset=['Time'])
+    df = df.dropna(subset=[time_column_name])
 
     # Split Name to Firstname and Lastname with ","
-    df[['Firstname','Lastname']] = df['Name'].str.split(',',expand=True)
+    if 'Name' in df.columns and ',' in df['Name'].iloc[1]:
+        df[['Firstname','Lastname']] = df['Name'].str.split(',',expand=True)
 
     return [excelExport.excel_export(df, "raceresult")]
 
