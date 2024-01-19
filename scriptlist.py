76db8d9a94c4fcb5809d8c505c9dd967c2482ee9
peyronth <peyron.th@gmail.com>
2024-01-19 11:11:15 +0100

    Add logicourse script for PDF parsing

--- scriptlist.py
+++ scriptlist.py
@@ -6,6 +6,7 @@ import scripts.ultratiming as ultratiming
 import scripts.utmb as utmb
 import scripts.yakachrono as yakachrono
 import scripts.livetrail as livetrail
+import scripts.logicourse as logicourse
 
 import tools.htmlParser.raceresult as HTMLraceresult
 
@@ -14,6 +15,7 @@ supportedWebsites = {
     'eventicom' : {"script": eventicom.eventicom, "exampleUrl": "https://eventicom.fr/resultats/TrailNoel2023/10%C3%A8me%20Trail%20de%20No%C3%ABl.clax" },
     'kikourou' : {"script": kikourou.kikourou, "exampleUrl": "http://www.kikourou.net/resultats/resultat-154252-la_course_nature_des_3_etangs_-_18_km-2020.html"},
     'livetrail' : {"script": livetrail.livetrail, "exampleUrl": "https://livetrail.net/histo/kosciuszko_2023/classement.php?course=100k&cat=scratch"},
+    'logicourse (pdf) [backyard]' : {"script": logicourse.logicourse_pdf_backyard, "exampleUrl": "https://www.logicourse.fr/images/newsite/manifestations/pollionnay/pollionnay2023resu.pdf"},
     'sportmaniacs' : {"script": sportmaniacs.sportmaniacs, "exampleUrl": "https://sportmaniacs.com/es/races/gtpe-2023-gran-trail-picos-de-europa/64976d6e-e580-48b6-ba51-6641ac1f1c02/results#rankings"},
     'ultratiming' : {"script": ultratiming.ultratiming, "exampleUrl": "https://www.ultratiming.be/evenement/trail-du-hoyoux-2023/epreuve/34km/resultats"},
     'utmb world' : {"script": utmb.utmbworld, "exampleUrl": "https://kosciuszko.utmb.world/runners/results?year=2023&raceUri=33948.ultra-trailkosciuszkobyutmbkoscimiler.2023"},
